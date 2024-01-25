4804565ebeb265a2d5c44d193367a4e93df6f763
Kaidi Zhang <kaidi.zhang1@singtel.com>
2024-01-25 23:16:50 +0800

    initialize project for cicd test

---
+++ src/main/java/sg/edu/nus/iss/cicdkaidi/CicdKaidiApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.cicdkaidi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdKaidiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdKaidiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/cicdkaidi/CicdKaidiApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.cicdkaidi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdKaidiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
